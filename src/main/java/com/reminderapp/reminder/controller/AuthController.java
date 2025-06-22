acaf36061e22a8e652db35e3126f8150f8212452
User S <sergeyrum101@gamil.com>
2025-06-22 14:57:36 +0300

    Authentication service and DTOs

--- src/main/java/com/reminderapp/reminder/controller/AuthController.java
+++ src/main/java/com/reminderapp/reminder/controller/AuthController.java
@@ -1,39 +1,26 @@
 package com.reminderapp.reminder.controller;
 
-import com.reminderapp.reminder.security.JwtUtil;
+import com.reminderapp.reminder.dto.AuthResponse;
+import com.reminderapp.reminder.dto.LoginRequest;
+import com.reminderapp.reminder.service.AuthorizationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/auth")
 @AllArgsConstructor
 public class AuthController {
-    private final AuthenticationManager authenticationManager;
-    private final UserDetailsService userDetailsService;
-    private final JwtUtil jwtUtil;
+    private final AuthorizationService authorizationService;
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> body) {
-        String email = body.get("email");
-        String password = body.get("password");
-
-        Authentication auth = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(email, password)
-        );
-        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-        String token = jwtUtil.generateToken(userDetails);
-
-        return ResponseEntity.ok(Map.of("token", token));
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authorizationService.login(loginRequest));
     }
+
 }
+
+
